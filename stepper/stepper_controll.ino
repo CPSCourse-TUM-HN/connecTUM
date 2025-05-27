ca884fc56cae39e2fb90b61631c5cf77c2f4b410
Julien-Alexandre Bertin Klein <jbertink@gmail.com>
2025-05-27 13:52:35 +0200

    merge arduino sketchs

--- stepper/stepper_controll.ino
+++ stepper/stepper_controll.ino
@@ -1,11 +1,21 @@
 #include "AccelStepper.h"
 #include "AccelStepperWithDistance.h"
+#include <Servo.h>
 
 #define dirPin 4
 #define stepPin 2
 #define buttonPin 14
 #define potPin 15
 #define motorInterfaceType 1
+
+#define unloadServoPin 9
+#define loadServoPin 10
+
+Servo loadServo;
+Servo unloadServo;
+
+int tileWidth=20; //mm
+int tileCount = 10;
 
 // to fix access to tty0
 // sudo chmod a+rw /dev/ttyUSB0
@@ -23,14 +33,33 @@ void setup() {
 
   pinMode(buttonPin, INPUT);
   pinMode(potPin, INPUT);
+
+  loadServo.attach(loadServoPin);
+  unloadServo.attach(unloadServoPin);
 }
 
-int pos = 0;
-int tileWidth=20; //mm
-int tileCount = 10;
 void loop() {
-  // Set the target position:
-  if(Serial.available()) pos = Serial.parseInt();
+  
+}
+
+void play(int pos) {
+  //Retreive coin
+  loadServo.write(180)
+  delat(250)
+  loadServo.write(0)
+  delay(50)
+
+  // Go to position
   stepper.runToNewDistance((pos*tileWidth)%(tileWidth*tileCount));
+  delay(50);
+
+  // Release coin
+  unloadServo.write(90)
+  delay(250)
+  unloadServo.write(0)
+  delay(50)
+
+  //Go back to home pos
+  stepper.runToNewDistance(0);
   delay(50);
 }
