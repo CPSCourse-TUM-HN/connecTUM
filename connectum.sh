a3841ecf6ee9427d52df901709b0d08f729b1337
pi <pi@raspberry.pi>
2025-07-19 03:56:11 +0200

    test webapp on pi

--- connectum.sh
+++ connectum.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Go into the environment
+source venv/bin/activate
+
 # Start backend (main.py)
-cd ~/Desktop/connectum || exit
-python3 main.py &
+cd ~/Desktop/test-ui || exit
+#gnome-terminal -- bash -c "python3 main.py --no-motors --no-camera; exec bash" &
+python3 main.py --no-motors --no-camera &
 
 # Start frontend in new terminal
-cd ~/Desktop/connectum-frontend || exit
+cd ~/Desktop/test-ui/connectum-frontend || exit
 gnome-terminal -- bash -c "npm run dev; exec bash"
 
 # Open browser to the frontend
-sleep 2
-xdg-open http://localhost:3000
+sleep 8
+chromium-browser --start-fullscreen http://localhost:3000
